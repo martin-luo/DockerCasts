ec31ad4edcaf903e768d3a878f950adcf0038859
An (Martin) Luo <aluo1.developer@gmail.com>
2020-12-03 17:55:21 +0800

    simple web server using docker

---
+++ my-solutions/simpleweb/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hi there");
+});
+
+app.get("/bye", (req, res) => {
+  res.send("Bye there");
+});
+
+app.listen(8080, () => {
+  console.log("Listening on port 8080");
+});
